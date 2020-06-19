d5cc9abb4e96fc010d855d37fab5db077aba73e0
Icey-HY <847443746@qq.com>
2020-06-19 16:22:06 +0800

    6.19 迁移学习图像分类

---
+++ jiangyuan/迁移学习/keras/图像分类.py
@@ -0,0 +1,227 @@
+# ----------------开发者信息--------------------------------
+# 开发者：姜媛
+# 开发日期：2020年6月19日
+# 修改日期：
+# 修改人：
+# 修改内容：
+# ----------------开发者信息--------------------------------
+
+
+# ----------------------   代码布局： ----------------------
+# 1、导入 Keras, matplotlib, numpy, sklearn 和 panda的包
+# 2、读取数据及与图像预处理
+# 3、迁移学习建模
+# 4、训练
+# 5、模型可视化与保存模型
+# 6、训练过程可视化
+# ----------------------   代码布局： ----------------------
+
+
+#  -------------------------- 1、导入需要包 -------------------------------
+from tensorflow.python.keras.utils import get_file
+import gzip
+import numpy as np
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import os
+from keras import applications
+import cv2
+import functools
+from keras.models import load_model
+import matplotlib.pyplot as plt
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # 使用第1个GPU
+#  -------------------------- 1、导入需要包 -------------------------------
+
+
+#  --------------------- 2、读取数据及与图像预处理 ---------------------
+path = 'C:\\Users\\HP\\Desktop\\每周代码学习\\迁移学习\\数据集'
+# 数据集与代码放在一起即可
+
+
+def load_data():
+    paths = [
+        path+'train-labels-idx1-ubyte.gz', path+'train-images-idx3-ubyte.gz',
+        path+'t10k-labels-idx1-ubyte.gz', path+'t10k-images-idx3-ubyte.gz'
+    ]
+
+    with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[1], 'rb') as imgpath:
+        x_train = np.frombuffer(
+            imgpath.read(), np.uint8, offset=16).reshape(len(y_train), 28, 28, 1)
+
+    with gzip.open(paths[2], 'rb') as lbpath:
+        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[3], 'rb') as imgpath:
+        x_test = np.frombuffer(
+            imgpath.read(), np.uint8, offset=16).reshape(len(y_test), 28, 28, 1)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+(x_train, y_train), (x_test, y_test) = load_data()  # read dataset
+batch_size = 32
+num_classes = 10
+epochs = 5
+data_augmentation = True  # 图像增强
+num_predictions = 20
+save_dir = os.path.join(os.getcwd(), 'saved_models_transfer_learning')
+model_name = 'keras_fashion_transfer_learning_trained_model.h5'
+
+
+# Convert class vectors to binary class matrices.  将类别弄成one-hot编码
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+# 由于mist的输入数据维度是(num, 28, 28)，vgg16 需要三维图像,因为扩充一下mnist的最后一维
+X_train = [cv2.cvtColor(cv2.resize(i, (48, 48)), cv2.COLOR_GRAY2RGB) for i in x_train]
+X_test = [cv2.cvtColor(cv2.resize(i, (48, 48)), cv2.COLOR_GRAY2RGB) for i in x_test]
+
+x_train = np.asarray(X_train)
+x_test = np.asarray(X_test)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255  # 归一化
+x_test /= 255  # 归一化
+#  --------------------- 2、读取数据及与图像预处理 ---------------------
+
+
+#  --------------------- 3、迁移学习建模 ---------------------
+# 使用VGG16模型 (include_top=False 表示 不包含最后的3个全连接层)
+base_model = applications.VGG16(include_top=False, weights='imagenet', input_shape=x_train.shape[1:])  # 第一层需要指出图像的大小
+print(x_train.shape[1:])
+
+# 建立CNN模型
+model = Sequential()
+print(base_model.output)
+model.add(Flatten(input_shape=base_model.output_shape[1:]))
+
+# 7 * 7 * 512 --> 256
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+# 256 --> num_classes
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+# add the model on top of the convolutional base
+# 输入为VGG16的数据，经过VGG16的特征层，2层全连接到num_classes输出（自己加的）
+model = Model(inputs=base_model.input, outputs=model(base_model.output))  # VGG16模型与自己构建的模型合并
+
+# 保持VGG16的前15层权值不变，即在训练过程中不训练
+for layer in model.layers[:15]:
+    layer.trainable = False
+
+# initiate RMSprop optimizer
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+# Let's train the model using RMSprop
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+#  --------------------- 3、迁移学习建模 ---------------------
+
+
+#  --------------------- 4、训练 ---------------------
+if not data_augmentation:
+    print('Not using data augmentation.')
+    history = model.fit(x_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        validation_data=(x_test, y_test),
+                        shuffle=True)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # 使输入数据集去中心化（均值为0），按feature执行
+        samplewise_center=False,  # 使输入数据的每个样本均值为0
+        featurewise_std_normalization=False,  # 将输入除以数据集的标准差来完成标准化，按feature执行
+        samplewise_std_normalization=False,  # 将输入的每个样本除以标准差
+        zca_whitening=False,  # 应用ZCA白化
+        # （假设训练数据是图像，由于图像中相邻像素之间具有很强的相关性，所以用于训练时输入是冗余的。白化的目的就是降低输入的冗余性。）
+        zca_epsilon=1e-06,  # epsilon for ZCA whitening
+        rotation_range=0,  # 整数，数据提升时图片随机转动的角度。随机选择图片的角度，是取值为0~180的度数
+        width_shift_range=0.1,  # 浮点数，图片宽度的某个比例，数据提升时图片随机水平偏移的幅度
+        height_shift_range=0.1,  # 浮点数，图片高度的某个比例，数据提升时图片随机竖直偏移的幅度。
+        shear_range=0.,  # 浮点数，剪切强度（逆时针方向的剪切变换角度）。是用来进行剪切变换的程度
+        zoom_range=0.,   # 用来进行随机的放大
+        channel_shift_range=0.,  # 浮点数，随机通道偏移的幅度
+        fill_mode='nearest',  # ‘constant’，‘nearest’，‘reflect’或‘wrap’之一，当进行变换时超出边界的点将根据本参数给定的方法进行处理
+        cval=0.,  # 浮点数或整数，当fill_mode=constant时，指定要向超出边界的点填充的值
+        horizontal_flip=True,   # 布尔值，进行随机水平翻转。随机的对图片进行水平翻转，这个参数适用于水平翻转不影响图片语义的时候
+        vertical_flip=False,  # 进行随机竖直翻转
+        rescale=None,  # 将在执行其他处理前乘到整个图像上，我们的图像在RGB通道都是0~255的整数，这样的操作可能使图像的值过高或过低，
+        # 所以我们将这个值定为0~1之间的数。
+        preprocessing_function=None,
+        # 将被应用于每个输入的函数。该函数将在任何其他修改之前运行。该函数接受一个参数，为一张图片（秩为3的numpy array），
+        # 并且输出一个具有相同shape的numpy array
+        data_format=None,
+        # 字符串，channels_last(默认)或channels_first之一，表示输入中维度的顺序。
+        # channels_last对应输入尺寸为(batch, height, width, channels)，
+        # channels_first对应输入尺寸为(batch, channels, height, width)。
+        # 它默认为从Keras配置文件~ /.keras / keras.json中找到的image_data_format值。
+        # 如果你从未设置它，将使用"channels_last"。
+        validation_split=0.0  # fraction of images reserved for validation (strictly between 0 and 1)
+    )
+
+    # Compute quantities required for feature-wise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+    print(x_train.shape[0]//batch_size)  # 取整
+    print(x_train.shape[0]/batch_size)  # 保留小数
+    # Fit the model on the batches generated by datagen.flow().
+    history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),  # 按batch_size大小从x,y生成增强数据
+                                  # flow_from_directory()从路径生成增强数据,和flow方法相比最大的优点在于不用
+                                  # 一次将所有的数据读入内存当中,这样减小内存压力，这样不会发生OOM
+                                  epochs=epochs,
+                                  steps_per_epoch=x_train.shape[0]//batch_size,
+                                  validation_data=(x_test, y_test),
+                                  workers=10  # 在使用基于进程的线程时，最多需要启动的进程数量。
+                                  )
+#  --------------------- 4、训练 ---------------------
+
+
+#  --------------------- 5、保存模型 ---------------------
+model.summary()
+# Save model and weights
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir, model_name)
+model.save(model_path)
+print('Saved trained model at %s ' % model_path)
+#  --------------------- 5、保存模型 ---------------------
+
+
+#  --------------------- 6、训练过程可视化 ---------------------
+# 绘制训练 & 验证的准确率值
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Valid'], loc='upper left')
+plt.savefig('tradition_cnn_valid_acc.png')
+plt.show()
+
+# 绘制训练 & 验证的损失值
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Valid'], loc='upper left')
+plt.savefig('tradition_cnn_valid_loss.png')
+plt.show()
+#  --------------------- 6、训练过程可视化 ---------------------
